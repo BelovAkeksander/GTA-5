487f19f7aa1c87c2b654edbf566e1e36ceefa51e
BelovAkeksander <147313344+BelovAkeksander@users.noreply.github.com>
2023-12-19 13:04:40 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 // Базовый класс Транспорт
 class Transport
@@ -65,23 +65,6 @@ class Helicopter : Transport
     }
 }
 
-//  Самолет
-class Airplane : Transport
-{
-    public string Airline { get; set; }
-
-    public Airplane(string model, string airline) : base(model)
-    {
-        Airline = airline;
-    }
-
-   
-    public override void StartEngine()
-    {
-        Console.WriteLine($"Двигатель самолета {Airline} {Model} запущен.");
-    }
-}
-
 //  Мопед
 class Moped : Transport
 {
@@ -96,6 +79,40 @@ class Moped : Transport
     public override void StartEngine()
     {
         Console.WriteLine($"Двигатель мопеда {Style} {Model} запущен.");
+    }
+}
+
+//  Самолет
+class Airplane : Transport
+{
+    public string Airline { get; set; }
+
+    public Airplane(string model, string airline) : base(model)
+    {
+        Airline = airline;
+    }
+
+
+    public override void StartEngine()
+    {
+        Console.WriteLine($"Двигатель самолета {Airline} {Model} запущен.");
+    }
+}
+
+// Трамвай
+class Tram : Transport
+{
+    public string City { get; set; }
+
+    public Tram(string model, string city) : base(model)
+    {
+        City = city;
+    }
+
+    // StartEngine
+    public override void StartEngine()
+    {
+        Console.WriteLine($"Двигатель трамвая  {City} {Model} запущен.");
     }
 }
 
@@ -116,22 +133,7 @@ class Motorboat : Transport
     }
 }
 
-// Трамвай
-class Tram : Transport
-{
-    public string City { get; set; }
 
-    public Tram(string model, string city) : base(model)
-    {
-        City = city;
-    }
-
-    // StartEngine
-    public override void StartEngine()
-    {
-        Console.WriteLine($"Двигатель трамвая в городе {City} {Model} запущен.");
-    }
-}
 
 class Program
 {
@@ -140,15 +142,15 @@ class Program
         // Создание объектов
         Transport[] vehicles = new Transport[]
         {
-            new Car("Civic", "Honda"),
+            new Car("Argetta", "Honda"),
             new Car("Camry", "Toyota"),
-            new Motorcycle("Ninja", "Sport"),
+            new Motorcycle("Bmw", "Sport"),
             new Motorcycle("Harley", "Cruiser"),
-            new Helicopter("H145", "Airbus"),
-            new Airplane("Boeing 747", "Delta"),
+            new Helicopter("H1000", "Airbus"),
+            new Airplane("IS", "t90"),
             new Moped("Vespa", "Classic"),
             new Motorboat("Speedster", "Speedboat"),
-            new Tram("T-1000", "New York")
+            new Tram("B-22", "Las Vegas")
         };
 
         // Запускаю двигатели с использованием цикла
